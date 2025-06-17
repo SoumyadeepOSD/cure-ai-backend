3b971b495f8eaf063a79634566b22033cc87ccf9
deepneuronsai <paper.project.2025@gmail.com>
2025-06-17 23:10:00 +0530

    Replaced old model with new model

--- app/model_loader.py
+++ app/model_loader.py
@@ -2,7 +2,7 @@ import os
 import tensorflow as tf
 
 # Path to the local model file (adjusted for your current placement)
-MODEL_LOCAL_PATH = "app/VGG16.h5"
+MODEL_LOCAL_PATH = "app/MobileNetV2_LungCancer.h5"
 
 def load_model_once():
     if not os.path.exists(MODEL_LOCAL_PATH):
